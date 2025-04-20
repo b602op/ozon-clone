6a65b26db94aef13588f7bd635cd84d80e0bd89a
Eduard Ispolatov <Eduard>
2025-04-20 23:20:41 +0300

    изменение стилей

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,31 @@
 import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
 
 export default function Home() {
   return (
-    <div>
-      <Button>
-        hello world
-      </Button>
+    <div className="p-4">
+      <div className="flex flex-col gap-y-4">
+        <div>
+          <Button variant="elevated">
+            я кнопка
+          </Button>
+        </div>
+        <div>
+          <Input placeholder="я инпут" />
+        </div>
+        <div>
+          <Progress value={50} />
+        </div>
+        <div>
+          <Textarea placeholder="я текстареа"/>
+        </div>
+        <div>
+          <Checkbox />
+        </div>
+      </div>
     </div>
   )
 }
